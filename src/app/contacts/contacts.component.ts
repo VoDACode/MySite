308373cceec38bde943715531023463027c3b877
VoDA <dimavovk201112@gmail.com>
2022-02-20 20:17:18 +0200

    Implemented sections "Contacts"

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css', '../../assets/css/content.css']
 })
 export class ContactsComponent implements OnInit {
 
@@ -11,5 +11,4 @@ export class ContactsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
